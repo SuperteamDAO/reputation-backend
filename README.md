6f2d0880fe6f29693e88e4b72c4ee3d32ccffa3a
Irfan Asif <irfan_2020bece060@nitsri.net>
2022-12-08 02:13:56 +0530

    ci: adds husky

---
+++ README.md
@@ -0,0 +1,10 @@
+<div align="center">
+
+  <img src="/public/superteamLogo.jpeg" width="140px" height="140px" />
+
+  <h1>Superteam Reputation</h1>
+
+  <p>
+    <strong>Reputation Dashboard for Superteam DAO</strong>
+  </p>
+</div>
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 // create a basic express server to ping the server
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (_req: any, res: { send: (arg0: string) => any }) =>
-  res.send('Hello World!')
-);
+app.get('/', (_req: any, res: { send: (arg0: string) => any }) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
